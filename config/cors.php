7fe629dc246eb46f0523c7a32c87ff26d8a70069
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-08-27 21:06:15 +0200

    move shape call to api

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['proxy/*', 'api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,2 @@
 <?php
 
-use App\Http\Controllers\ElectricSqlProxyController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/proxy/v1/shape', ElectricSqlProxyController::class)
-    ->name('electric.shape.proxy');
