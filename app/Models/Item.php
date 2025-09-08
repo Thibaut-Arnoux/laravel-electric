1ec20ee7d908921a31e2f5e39869dd22e584be29
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-09-08 21:41:20 +0200

    add items endpoint and upgrade new trait

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,18 +2,23 @@
 
 namespace App\Models;
 
+use App\Traits\ElectricAttributes;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
 
 class Item extends Model
 {
+    use ElectricAttributes;
     use HasTranslations;
     use HasUuids;
 
     protected $casts = ['spawns' => 'array'];
 
     protected $guarded = ['id'];
+
+    /** @var string[] */
+    protected static array $shape = ['*'];
 
     /** @var string[] */
     public array $translatable = ['name', 'description'];
--- app/Traits/ElectricAttributes.php
+++ app/Traits/ElectricAttributes.php
@@ -2,11 +2,34 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Schema;
+
 trait ElectricAttributes
 {
+    /** @return string[] */
+    public static function getTableColumns(): array
+    {
+        return Schema::getColumnListing((static::make())->getTable());
+    }
+    /**
+     * The attributes authorized and exported by default to Eletric.
+     *
+     * @return string[]
+     */
     public static function getShape(): array
     {
-        return is_array(static::$shape) ? static::$shape : [];
+        // use property_exists and not is_array as spatie does for translation because it is static and not instance property
+        if (property_exists(static::class, 'shape') === false) {
+            return [];
+        }
+
+        /** @disregard P1014 */
+        if (static::$shape === ['*']) {
+            return static::getTableColumns();
+        }
+
+        /** @disregard P1014 */
+        return static::$shape;
     }
 
     public static function getImplodedShape(string $separator = ','): string
