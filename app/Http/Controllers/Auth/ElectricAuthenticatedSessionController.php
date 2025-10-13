a8019c226719455741752aa1dac27ffd65f72088
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-10-13 14:09:41 +0200

    setup sanctum with one fake user

---
+++ app/Http/Controllers/Auth/ElectricAuthenticatedSessionController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\GenericUser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class ElectricAuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        Auth::login(new GenericUser([
+            'id' => 1,
+        ]));
+
+        $request->session()->regenerate();
+
+        return response()->json(status: Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\ElectricAuthenticatedSessionController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [ElectricAuthenticatedSessionController::class, 'store'])
+    ->name('login')
+    ->middleware('guest');
