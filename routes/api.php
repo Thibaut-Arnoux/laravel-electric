ebf30d7d650b08abb2644990ddc3fe10790c819e
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-10-13 14:09:41 +0200

    move shape call to api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ElectricSqlProxyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('shape')->group(function () {
+    Route::get('/users', ElectricSqlProxyController::class)->name('shape.users.index');
+});
