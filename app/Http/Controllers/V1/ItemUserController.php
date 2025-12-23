2e3f546a22eadf1a71e9d3b3198befa56b18f01e
Thibaut-Arnoux <thib.arnoux@gmail.com>
2025-12-23 16:26:18 +0100

    refactor: use Responsable

--- app/Http/Controllers/V1/ItemUserController.php
+++ app/Http/Controllers/V1/ItemUserController.php
@@ -11,6 +11,7 @@ use App\Http\Requests\V1\UpdateItemUserRequest;
 use App\Http\Responses\V1\ElectricResponse;
 use App\Models\ItemUser;
 use App\Services\ElectricService;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
@@ -27,13 +28,11 @@ class ItemUserController extends Controller
     /**
      * Store a newly created item-user relationship.
      */
-    public function store(StoreItemUserRequest $request, StoreItemUserAction $action): JsonResponse
+    public function store(StoreItemUserRequest $request, StoreItemUserAction $action): Responsable
     {
-        $response = new ElectricResponse(
+        return new ElectricResponse(
             callback: fn () => $action($request->validated())
         );
-
-        return $response->toResponse($request);
     }
 
     /**
@@ -43,11 +42,9 @@ class ItemUserController extends Controller
         ItemUser $item,
         UpdateItemUserRequest $request,
         UpdateItemUserAction $action
-    ): JsonResponse {
-        $response = new ElectricResponse(
+    ): Responsable {
+        return new ElectricResponse(
             callback: fn () => $action($item, $request->validated())
         );
-
-        return $response->toResponse($request);
     }
 }
